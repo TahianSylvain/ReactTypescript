b9735369cce6907eaec45478b1812f56f583b8ba
Sylvain <124794207+TahianSylvain@users.noreply.github.com>
2024-03-19 07:43:32 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+// import sys;
 import { Container } from './Components/StyleProp';
 import { Button, Input } from './Components/EventProps';
 import { Status } from './Components/Status';
